1a4b380562942fa66e41250a17d7861296ae4e33
app182019 <47757230+app182019@users.noreply.github.com>
2019-02-18 21:20:10 +0200

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,44 @@
+
+// program 1
+// start
+
+console.clear()
+console.log("Hello world!") // output
+
+// stop
+
+// program 2
+// start
+
+// x = 5 * 5
+var x = 0
+x = 0
+x = 5 * 5
+console.log(x)
+x = 5 * 6
+
+// stop
+ 
+// program 3
+// start
+
+var y = prompt("Please enter number", 0)
+console.log(y * 2)
+
+// stop
+
+// program 4
+// start
+
+
+var a = prompt("Enter 1st number:", 0)
+var b = prompt("Enter 2st number:", 0)
+
+if (a == b)
+	console.log("equal")
+else
+  console.log("not equal")
+
+// stop
+
+
